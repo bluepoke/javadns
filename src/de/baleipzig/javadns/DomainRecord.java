4d974d421a91cd4f804d111dafb34bfe2a321e0d
Nils Foken <nf462138@gmail.com>
2012-02-08 11:49:57 +0100

    licensing info

--- src/de/baleipzig/javadns/DomainRecord.java
+++ src/de/baleipzig/javadns/DomainRecord.java
@@ -1,3 +1,32 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011  Nils Foken, André Kießlich,                        *
+ *                       Peter Kossek, Hans Laser                           *
+ *                                                                          *
+ *  Nils Foken       <nils.foken@it2009.ba-leipzig.de>                      *
+ *  André Kießlich   <andre.kiesslich@it2009.ba-leipzig.de>                 *
+ *  Peter Kossek     <peter.kossek@it2009.ba-leipzig.de>                    *
+ *  Hans Laser       <hans.laser@it2009.ba-leipzig.de>                      *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'javadns'.                                         *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
 package de.baleipzig.javadns;
 
 import java.util.HashMap;
@@ -12,12 +41,6 @@ import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 
 public class DomainRecord {
-
-    public static final String RECORD_A = "A";
-    public static final String RECORD_SOA = "SOA";
-    public static final String RECORD_NS = "NS";
-    public static final String RECORD_MX = "MX";
-    public static final String RECORD_CNAME = "CNAME";
     
     private static HashMap<String, HashMap<String, Attribute>> records 
     	= new HashMap<String, HashMap<String, Attribute>>();
